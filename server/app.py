6a8e45411828b6715af5aecdf6bbbc67de624caa
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-04-07 16:08:08 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -42,10 +42,35 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
+    
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        data = request.get_json()
+
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
